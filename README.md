a084760f1a075bd671f0af4a46fe7878eeafeafc
Stijn Mathysen <stijn@skylight.be>
2025-09-14 21:16:39 +0200

    skeleton setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# okaeri
+
+## Setup
+
+```bash
+curl -fsSL https://raw.githubusercontent.com/Skylight/okaeri/refs/heads/main/setup.sh | bash
+```
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if ! type -p git >/dev/null; then
+	sudo apt install git -y git
+fi
+
+if [ ! -d "$HOME/okaeri" ]; then
+	echo "Setting up repository"
+	cd $HOME
+	git clone git@github.com:Skylight/okaeri.git
+fi
+
+grep -qxF '. okaeri/core/bash/initialize' $HOME/.bashrc || printf "\n\n# Okaeri (https://github.com/Skylight/okaeri)\n. okaeri/core/bash/initialize\n" >> $HOME/.bashrc
+
+echo ""
+echo "Okaeri installed"
+echo "================"
+echo ""
+echo "The next time you connect to the terminal, the boot script will be loaded. To run the boot script now, type:"
+echo ""
+echo ". okaeri/core/bash/initialize"
+echo ""
